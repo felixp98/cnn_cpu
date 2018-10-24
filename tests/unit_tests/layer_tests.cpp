72fe66df8e54088e75b70983a67efbf6f99cb4c9
Felix Preuschoff <it16111@lehre.dhbw-stuttgart.de>
2018-10-24 18:01:25 +0200

    refactored project structure (CMake structure)

---
+++ tests/unit_tests/layer_tests.cpp
@@ -0,0 +1,13 @@
+//
+// Created by fpreuschoff on 23.10.2018.
+//
+
+#include "gtest/gtest.h"
+
+TEST(layer_tests, first_test) {
+    EXPECT_EQ(0, 1);
+}
+
+TEST(layer_tests, second_test){
+    EXPECT_EQ(1,1);
+}
